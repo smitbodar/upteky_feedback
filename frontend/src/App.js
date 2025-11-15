399dbb3cb73be98a64a2ec4f04fa1daa456ec408
smitbodar <smitbodar123@gmail.com>
2025-11-15 14:45:24 +0530

    update API URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import StarRating from "./StarRating";
 import "./App.css";
 
 function App() {
-  const API_URL = "https://upteky-feedback.onrender.com/api/feedback"; 
+  const API_URL = "https://upteky-feedback-1.onrender.com/api/feedback"; 
 
   const [feedbacks, setFeedbacks] = useState([]);
   const [form, setForm] = useState({
